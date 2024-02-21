1e0a6dd1bb75a7a2f023fd74fb0837d7ab9f4ecc
thonyakina <thonyakina@zuzex.com>
2024-02-21 12:50:35 +0300

    Создала отдельный компонент для поиска города и его погоды, перенесла логику поиска в него

--- src/Components/HomeScreen.jsx
+++ src/Components/HomeScreen.jsx
@@ -1,56 +1,32 @@
 import {useState} from "react";
-import axios from "axios";
 
 import WeatherForecastTable from "./WeatherForecastTable";
+import SearchCityWeather from "./SearchCityWeather";
 
 const HomeScreen = () => {
-    const [location, setLocation] = useState('Lisbon');
+    const [displayLocation, setDisplayLocation] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [weather, setWeather] = useState({});
 
-    async function weatherSelection() {
-        try {
-            setIsLoading(true);
-
-            const geoData = await axios(
-                `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-            )
-                .then((response) => response.data.results);
-
-            const { latitude, longitude, timezone, name, countryCode } = geoData[0];
-
-            const weatherData = await axios(
-                `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weather_code,temperature_2m_max,temperature_2m_min`
-            )
-                .then((response) => response.data.daily);
-
-            setWeather(weatherData);
-        } catch (err) {
-            console.log(err)
-        } finally {
-            setIsLoading(false);
-        }
-    }
-
     return (
         <div className='home_container'>
-            <h1 className='home_title'>Classy Weather</h1>
-            <input
-                className='form_find-city'
-                type="text"
-                placeholder='Find city...'
-                value={location}
-                onChange={e => setLocation(e.target.value)}
+            <SearchCityWeather
+                setDisplayLocation={setDisplayLocation}
+                setIsLoading={setIsLoading}
+                setWeather={setWeather}
             />
-            <button onClick={weatherSelection}>Get weather</button>
 
             {isLoading &&
                 <p className='loader'>Loading...</p>
             }
 
-            <div className='weather_forecast-block'>
-                <WeatherForecastTable/>
-            </div>
+            {weather?.weather_code &&
+                <div className='weather_forecast-block'>
+                    <WeatherForecastTable
+                        {...weather}
+                        location={displayLocation}
+                    />
+                </div>}
         </div>
     )
 };
---
+++ src/Components/SearchCityWeather.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useState } from "react";
+
+const SearchCityWeather = ({ setDisplayLocation, setIsLoading, setWeather }) => {
+    const [location, setLocation] = useState('Lisbon');
+
+    async function weatherSelection() {
+        try {
+            setIsLoading(true);
+
+            const geoData = await axios(
+                `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+            )
+                .then((response) => response.data.results);
+
+            const { latitude, longitude, timezone, name } = geoData[0];
+            setDisplayLocation(name);
+
+            const weatherData = await axios(
+                `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weather_code,temperature_2m_max,temperature_2m_min`
+            )
+                .then((response) => response.data.daily);
+
+            setWeather(weatherData);
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return (
+        <>
+            <h1 className='home_title'>Classy Weather</h1>
+            <input
+                className='form_find-city'
+                type="text"
+                placeholder='Find city...'
+                value={location}
+                onChange={e => setLocation(e.target.value)}
+            />
+            <button onClick={weatherSelection}>Get weather</button>
+        </>
+    )
+};
+
+export default SearchCityWeather;
