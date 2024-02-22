21be98e8ecad3e60d8680351011d42fb57e83709
thonyakina <thonyakina@zuzex.com>
2024-02-22 12:10:48 +0300

    Удален класс

--- src/Class/Weather.js
+++
@@ -1,8 +0,0 @@
-export default class Weather {
-    constructor(temperatureMax, temperatureMin, time, code) {
-        this.temperatureMax = temperatureMax;
-        this.temperatureMin = temperatureMin;
-        this.date = time;
-        this.code = code;
-    }
-};
